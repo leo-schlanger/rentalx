9787c1d69a1d2e8c67735d095abbb2ab68c8ec1b
leo-schlanger <leoschlanger@gmail.com>
2021-03-23 07:22:07 -0300

    docker and postgres db

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -2,12 +2,13 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+export default (): ImportCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
 
-const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+  const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
-const importCategoryController = new ImportCategoryController(
-  importCategoryUseCase
-);
-
-export { importCategoryController };
+  const importCategoryController = new ImportCategoryController(
+    importCategoryUseCase
+  );
+  return importCategoryController;
+};

7bc2d0c2743419cc6d7fb9d09bae47c3d89b42a1
leo-schlanger <leoschlanger@gmail.com>
2021-03-23 14:50:30 -0300

    user implementation, authentication and erros

---
+++ src/modules/accounts/UseCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const token = sign({}, "d323ef6e92cf06d5af36be6864446a72", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
